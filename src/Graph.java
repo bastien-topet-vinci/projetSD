a5aefed6bb664cb0f34d8ce272a5bbd4c68a2800
Topet <bastien.topet@student.vinci.be>
2024-03-01 14:10:04 +0100

    add : setup of the project

---
+++ src/Graph.java
@@ -0,0 +1,12 @@
+public class Graph {
+
+private
+
+  public void calculerItineraireMinimisantNombreRoutes(String ville1, String ville2) {
+
+  }
+
+  public void calculerItineraireMinimisantKm(String ville1, String ville2) {
+
+  }
+}
