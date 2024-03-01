c8ac3b505e5185d6f213d1625ded8570a646d27e
TiagoAmaralAndre <tiagolopes970@gmail.com>
2024-03-01 14:23:41 +0100

    classe graph

--- src/Graph.java
+++ src/Graph.java
@@ -1,6 +1,18 @@
+import java.io.File;
+
 public class Graph {
 
-private
+private int[][] matriceAdjacence;
+private File cities;
+private File roads;
+private static int  nbVilles = 0;
+
+
+
+    public Graph(File cities, File roads) {
+      this.cities = cities;
+      this.roads = roads;
+    }
 
   public void calculerItineraireMinimisantNombreRoutes(String ville1, String ville2) {
 
