e93142ce532cdc95cb5405d2c055ed22275bc63d
TiagoAmaralAndre <tiagolopes970@gmail.com>
2024-03-01 15:41:17 +0100

    lire fichier city

--- src/Graph.java
+++ src/Graph.java
@@ -1,5 +1,9 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.util.HashMap;
+
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -13,6 +17,23 @@ public class Graph {
     this.cities = cities;
     this.roads = roads;
     trajetVilleRoute = new HashMap<String, Set<Road>>();
+
+    try {
+      BufferedReader lecteur = new BufferedReader(new FileReader(cities));
+      String ligne;
+      while ((ligne = lecteur.readLine()) != null) {
+        String[] ville = ligne.split(",");
+        if (ville.length >= 4) {
+          City city = new City(Integer.parseInt(ville[0]), ville[1],  Double.parseDouble(ville[2]),  Double.parseDouble(ville[3]));
+          trajetVilleRoute.put(city.getNom(), new HashSet<Road>());
+        }
+      }
+    } catch (Exception e) {
+      throw  new RuntimeException("Erreur lors de la lecture du fichier cities.txt");
+    }
+
+
+
   }
 
   public void calculerItineraireMinimisantNombreRoutes(String ville1, String ville2) {
